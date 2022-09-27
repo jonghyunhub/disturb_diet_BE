de258c90aada3ee299db005f094f2af1d20effbb
chujonghyun <chuwhdgus@gmail.com>
2022-09-27 11:21:16 +0900

    first commit

---
+++ src/test/java/com/example/disturb_diet/DisturbDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disturb_diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisturbDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
